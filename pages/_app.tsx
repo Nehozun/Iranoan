835db4dc03dd265750813862103f9d11821c8ec0
Nehozun <57873399+Nehozun@users.noreply.github.com>
2021-05-30 10:32:24 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Iranoan official website." />
       <title>Iranoan</title>
-      <link rel="icon" type="image/png" href="en/favicon.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Noto+Sans+JP:wght@400;500&family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
     </Head>
