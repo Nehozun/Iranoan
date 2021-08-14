f94e08e329b9011a791e86690055afafc946eb2d
Nehozun <lighthouse1910@outlook.com>
2021-08-14 11:35:42 +0700

    chang tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "@styles/global.scss";
-import "antd/dist/antd.css";
 import { DefaultSeo } from "next-seo";
 import { seoConfig } from "next-seo.config";
 import { AppProps } from "next/app";
