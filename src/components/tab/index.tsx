96359e4494b5a7f4b8a3101e732ee427e9bfae19
Nehozun <lighthouse1910@outlook.com>
2021-09-19 20:19:12 +0700

    new tab

--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -5,7 +5,11 @@ const PageTab: React.FC<{
   content: any;
   isBigImage?: boolean;
   link?: string;
+  isNew?: boolean;
 }> = (props) => {
+  const onClickImage = () => {
+    window.open(props.link, "_blank");
+  };
   return (
     <div className="page-tab flex items-center justify-between font-notoSans gap-8 flex-wrap-reverse">
       <div
@@ -14,6 +18,12 @@ const PageTab: React.FC<{
           (props.isBigImage ? "big-img-container" : "")
         }
       >
+        {props.isNew ? (
+          <img
+            src="images/new-tag.png"
+            className="w-13 md:w-20 hidden md:block"
+          ></img>
+        ) : null}
         <h2 className="tab-title font-bold text-2xl md:text-5xl text-third hidden md:block">
           {props.link ? (
             <a href={props.link} target="_blank">
@@ -42,12 +52,20 @@ const PageTab: React.FC<{
       </div>
       <img
         src={props.imageUrl}
+        onClick={onClickImage}
         alt="tab"
         className={
-          "tab-image object-contain " + (props.isBigImage ? " big-img" : "")
+          "tab-image cursor-pointer object-contain " +
+          (props.isBigImage ? " big-img" : "")
         }
       />
-      <h2 className="tab-title font-bold text-2xl md:text-5xl text-third block md:hidden">
+      <h2 className="tab-title font-bold text-2xl md:text-5xl text-third block md:hidden w-full">
+        {props.isNew ? (
+          <img
+            src="images/new-tag.png"
+            className="w-13 md:w-20 block md:hidden"
+          ></img>
+        ) : null}
         {props.link ? (
           <a href={props.link} target="_blank">
             {props.title}
