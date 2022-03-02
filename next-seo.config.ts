7279940f00524af24a3c0770e9bb191540b7f8ef
Nehozun <lighthouse1910@outlook.com>
2022-03-02 18:48:44 +0700

    uupdate news

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,13 +1,13 @@
 import type { DefaultSeoProps } from 'next-seo';
 
 export const seoConfig: DefaultSeoProps = {
-  title: 'Iranoan',
-  titleTemplate: '%s - Iranoan Site',
+  title: 'Iranoan(イラノアン)',
+  titleTemplate: '%s - Iranoan(イラノアン)',
   description: 'イラノアンの公式ホームページです。トレーディングカード販売、アプリ開発をはじめ、あなたのライフスタイルに刺激と遊びを与えるサービスを開発しています。',
   openGraph: {
     type: 'website',
     locale: 'ja_JP',
-    site_name: 'Iranoan',
+    site_name: 'Iranoan(イラノアン)',
   },
   twitter: {
     handle: '@handle',
